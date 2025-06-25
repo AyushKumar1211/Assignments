6c735e456a83225beb6a9b7c50975e7d5c9aab93
AyushKumar1211 <145289543+AyushKumar1211@users.noreply.github.com>
2025-06-25 23:30:52 +0530

    Add files via upload

---
+++ Day10_Assignment/script_ques4.js
@@ -0,0 +1,7 @@
+const ul = document.getElementById("shopping-list");
+const items = ["Item A", "Item B", "Item C"];
+items.forEach(itemText => {
+    const li = document.createElement("li");
+    li.textContent = itemText;
+    ul.appendChild(li);
+});
